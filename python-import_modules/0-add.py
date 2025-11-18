efd8dba82dc2cfbab3ba2d1032b95dcceb1eb520
Your Name <you@example.com>
2025-11-18 10:39:54 +0000

    h

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,7 +2,7 @@
 add_0 = __import__("add_0")
 add = add_0.add
 
-a = 1
-b = 2
-
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
