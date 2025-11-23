932a9a7e16c88aba9b95f899f63424623dac9e36
Your Name <you@example.com>
2025-11-23 18:05:30 +0000

    sss

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
+    printed = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
+            value = my_list[i]
+            print("{:d}".format(value), end="")
+            printed += 1
         except (TypeError, ValueError):
-            pass
+            continue
         except IndexError:
             break
     print()
-    return count
+    return printed
