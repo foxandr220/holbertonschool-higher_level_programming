92c44929a88a1006e31ecfa40736b1d08ba99000
Your Name <you@example.com>
2025-11-28 12:21:54 +0000

    d

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Function that returns the list of available attributes and methods of an object."""
-
+    
 
 def lookup(obj):
     """Return the list of available attributes and methods of an object."""
     return dir(obj)
+
