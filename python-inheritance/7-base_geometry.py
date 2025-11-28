f32ddc337051582a3238245496b94bd840a6fabf
Your Name <you@example.com>
2025-11-28 13:24:47 +0000

    d

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -6,12 +6,28 @@ class BaseGeometry:
     """A class representing base geometry"""
 
     def area(self):
-        """Calculate area of geometry"""
+        """Calculate area of geometry
+
+        Raises:
+            Exception: area() is not implemented
+        """
         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
-        """Validate integer value"""
-        if type(value) not in (int,) or isinstance(value, bool):
+    def integer_validator(self, name, value=None):
+        """Validate integer value
+
+        Args:
+            name (str): The name of the value
+            value: The value to validate
+
+        Raises:
+            TypeError: If value is not an integer or missing
+            ValueError: If value is less or equal to 0
+        """
+        if value is None:
+            raise TypeError("integer_validator() missing required positional argument: 'value'")
+        
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
