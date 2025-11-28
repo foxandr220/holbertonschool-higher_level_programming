3098114e95fa644b7f616650d3f7686fff2ff70a
Your Name <you@example.com>
2025-11-28 12:41:12 +0000

    d

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Module that defines the MyList class."""
+"""MyList class that inherits from list"""
 
 
 class MyList(list):
-    """A class that inherits from list."""
-
+    """A class that inherits from list"""
+    
     def print_sorted(self):
-        """Print the list in ascending sorted order."""
+        """Prints the list sorted in ascending order"""
         print(sorted(self))
