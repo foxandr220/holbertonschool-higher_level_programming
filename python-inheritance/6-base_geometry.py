064231925fd9bbffa81a1161f8fbafdedb55506d
Your Name <you@example.com>
2025-11-28 13:04:07 +0000

    d

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""BaseGeometry class with area method"""
+
+
+class BaseGeometry:
+    """A class representing base geometry"""
+
+    def area(self):
+        """Calculate area of geometry
+
+        Raises:
+            Exception: area() is not implemented
+        """
+        raise Exception("area() is not implemented")
