cce2d42b074e9a1acd48be945b78953384bb9e67
Your Name <you@example.com>
2025-11-28 12:53:28 +0000

    d

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,11 +4,11 @@
 
 def is_same_class(obj, a_class):
     """Check if obj is exactly an instance of a_class
-    
+
     Args:
         obj: The object to check
         a_class: The class to check against
-        
+
     Returns:
         bool: True if obj is exactly an instance of a_class, False otherwise
     """
