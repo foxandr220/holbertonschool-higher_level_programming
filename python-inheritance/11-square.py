2b5b983d0b7495beb39b13c2ce52cb388a721aee
Your Name <you@example.com>
2025-11-28 14:06:38 +0000

    d

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Square class that inherits from Rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class representing a square"""
+
+    def __init__(self, size):
+        """Initialize Square with size
+
+        Args:
+            size (int): The size of the square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Return string representation of the square
+
+        Returns:
+            str: The square description
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
