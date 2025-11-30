0209460583969e108489af0a5377fc397152b7a0
Your Name <you@example.com>
2025-11-30 11:34:49 +0000

    hotfix

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- lists all cities and their states sorted by cities.id
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
