561d477cad6ac85528ffe8379fc295da259474fb
Your Name <you@example.com>
2025-11-30 09:42:21 +0000

    hotfix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,12 @@
-  GNU nano 6.2                                                                                                                7-add_item.py                                                                                                                         
 #!/usr/bin/python3
 """
-Script that adds all arguments to a Python list and saves them to a file.
+Script that adds all arguments to a Python list,
+and then saves them to a file.
 """
 
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
-
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
