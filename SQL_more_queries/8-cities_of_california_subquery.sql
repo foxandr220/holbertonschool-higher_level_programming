790a7cbb8c26efd9d94af2f5f1318f0c7460cc61
Your Name <you@example.com>
2025-11-30 11:33:51 +0000

    hotfix

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- lists all cities of California using a subquery
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
+ORDER BY cities.id ASC;
