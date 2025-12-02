7343d3d268ca56b223e30f5413db5678959653bb
Your Name <you@example.com>
2025-12-02 11:38:01 +0000

    hotfix

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""Fetch X-Request-Id from response header"""
+
 import urllib.request
 import sys
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
     with urllib.request.urlopen(url) as response:
-        headers = response.headers
-        print(headers.get("X-Request-Id"))
+        print(response.headers.get("X-Request-Id"))
